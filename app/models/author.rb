bf7158f795a9eec6bccacf486d7cc11e889636b4
Marc Blümner <m.bluemner@liquidlabs.de>
2014-08-04 13:38:49 +0200

    nested attributes for books and addresses

--- app/models/author.rb
+++ app/models/author.rb
@@ -3,6 +3,11 @@ class Author
   include Mongoid::Attributes::Dynamic
 
   field :name, type: String
+  field :last_name, type: String
 
+
+  has_many :books
   embeds_one :address
+
+  accepts_nested_attributes_for :books, :address
 end
