9c556343a3e9fcb9dd7933dfdcab56e7c44829c3
Marc Blümner <m.bluemner@liquidlabs.de>
2014-08-04 12:00:13 +0200

    setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sodibee::Application.config.secret_key_base = '61d2e60279d165baa39cb219f81d14905409f322576711f724b4118fd25e498b9802d9af2cd1fdf47316cd3dea82a02fea04d083730e832840eef000d81c9384'
